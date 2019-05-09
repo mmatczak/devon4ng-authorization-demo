ff076a95d1169d12845dc39bb39b991456d9f88b
mmatczak <marek.matczak@capgemini.com>
2019-05-09 22:54:12 +0200

    link to the home page added

--- src/app/components/dialog2.component.ts
+++ src/app/components/dialog2.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dialog2',
-  template: '<h2>Dialog 2</h2>'
+  template: `
+    <h2>Dialog 2</h2>
+    <p>Back to <a routerLink="/dialog1">home</a></p>`
 })
 export class Dialog2Component {
 }
